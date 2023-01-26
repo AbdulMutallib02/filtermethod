c6259c52a10e99b5d8bf124734e1758c8591cbbe
Mutallib <qmutallib2002@gmail.com>
2023-01-26 17:44:57 +0530

    Initial commit

---
+++ src/Components/Categories.js
@@ -0,0 +1,110 @@
+const Categories = [
+    {
+        id: 1,
+        title: "Black t-Shirt",
+        price: 200,
+        category: "men",
+        image: "https://5.imimg.com/data5/GX/XW/MY-67450080/mens-black-plain-t-shirt-1000x1000.jpg"
+    },
+    {
+        id: 2,
+        title: "Red Shirt",
+        price: 200,
+        category: "men",
+        image: "https://rukminim1.flixcart.com/image/832/832/kmp7ngw0/shirt/0/y/w/m-j316932xuu-calvin-klein-jeans-original-imagfjfshfvwyqwa.jpeg?q=70"
+    },
+    {
+        id: 3,
+        title: "Kurti",
+        price: 200,
+        category: "Women",
+        image: "https://cdn.shopify.com/s/files/1/0275/0713/0416/products/K1533_1_790x1280.jpg?v=1659540895"
+    },
+    {
+        id: 4,
+        title: "Saree",
+        price: 200,
+        category: "Women",
+        image: "https://assets.myntassets.com/h_1440,q_90,w_1080/v1/assets/images/14975358/2021/7/29/9e1484a0-92f8-4871-9b98-fc9f389406691627570339977KALINIWomenBlackPrinted1.jpg"
+    },
+    {
+        id: 5,
+        title: "Salwar-Suit",
+        price: 200,
+        category: "Women",
+        image: "https://i.etsystatic.com/32035316/r/il/6ee4a3/3361527942/il_1140xN.3361527942_eipx.jpg"
+    },
+    {
+        id: 6,
+        title: "Jaggins",
+        price: 200,
+        category: "Women",
+        image: "https://cdn.shopify.com/s/files/1/0399/0231/4646/products/SFPANT5411-1_720x.jpg?v=1666349662"
+    },
+    {
+        id: 7,
+        title: "Suits",
+        price: 200,
+        category: "men",
+        image: "https://imagescdn.planetfashion.in/img/app/product/6/636736-6307679.jpg?auto=format"
+    },
+    {
+        id: 8,
+        title: "Frocks",
+        price: 200,
+        category: "children",
+        image: "https://static3.azafashions.com/tr:w-575,dpr-2,e-sharpen/uploads/product_gallery/1633955-0217010001654251321.jpg"
+    },
+    {
+        id: 9,
+        title: "T-Shirt",
+        price: 200,
+        category: "children",
+        image: "https://5.imimg.com/data5/FH/OJ/MY-9097565/color-t-shirt-1000x1000.jpg"
+    },
+    {
+        id: 10,
+        title: "men's Black Shirt",
+        price: 200,
+        category: "men",
+        image: "https://sslimages.shoppersstop.com/sys-master/images/h07/h61/27963089518622/A22SFCSLPT00894_BLACK_alt2.jpg_2000Wx3000H"
+    },
+    {
+        id: 11,
+        title: "white saree",
+        price: 200,
+        category: "women",
+        image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7M9F5SrAZ_MP1t8Tr_kDxWX9DR4pQi19Suw&usqp=CAU"
+    },
+    {
+        id: 12,
+        title: "Jeans",
+        price: 200,
+        category: "children",
+        image: "https://www.beyoung.in/api/cache/catalog/products/men_new_jeans/bold_ink_blue_regular_fit_jeans_base_700x933.jpg"
+    },
+    {
+        id: 13,
+        title: "Trousers",
+        price: 200,
+        category: "children",
+        image: "https://cdn.shopify.com/s/files/1/2526/8658/products/1_eb927c67-7db1-40dc-b5fe-6d28bc249d53.jpg?v=1650959060"
+    },
+    {
+        id: 14,
+        title: "Shorts",
+        price: 200,
+        category: "men",
+        image: "https://images.meesho.com/images/products/79111863/fbi4x_256.jpg"
+    },
+    {
+        id: 15,
+        title: "Casuals",
+        price: 200,
+        category: "men",
+        image: "https://cdn.luxe.digital/media/2019/09/12085003/casual-dress-code-men-style-summer-luxe-digital.jpg"
+    },
+
+]
+
+export default Categories;
---
+++ src/Components/Category.js
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import Categories from "./Categories";
+
+const Category = () => {
+    const [data, setData] = useState(Categories);
+
+    const filterResult = (cartItems) =>{
+        const result = Categories.filter((currData)=>{
+          return  currData.category === cartItems
+        })
+        setData(result)
+    }
+  return (
+    <>
+      <h1 className="text-center text-info">Let's Shop</h1>
+      <div className="container-fluid mx-2">
+        <div className="row mt-5 mx-2">
+          <div className="col-md-3">
+            <button className="btn btn-warning w-100 mb-4"onClick={()=>filterResult("men")}>Men</button>
+            <button className="btn btn-warning w-100 mb-4"onClick={()=>filterResult("women")}>Women</button>
+            <button className="btn btn-warning w-100 mb-4"onClick={()=>filterResult("children")}>Children</button>
+            <button className="btn btn-warning w-100 mb-4" onClick={()=>setData(Categories)} >All</button>
+          </div>
+
+          <div className="col-md-9">
+            <div className="row">
+                {data.map((val)=>{
+                    const {id, title, price, image} = val
+                    return (
+                        <>
+                        <div className="col-md-4 mb-4" key={id}>
+                <div className="card">
+                  <img src={image} className="card-img-top" alt="..." />
+                  <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p>Price: {price}/-</p>
+                    <p className="card-text">
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                    <button className="btn btn-dark">
+                      Buy Now
+                    </button>
+                  </div>
+                </div>
+              </div>
+                        </>
+                    )
+                })}
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Category;
